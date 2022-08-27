3537cd8944b7f7211cb2129ad1b76397f2f76ce4
kidp2h <thjnhsoajca@gmail.com>
2022-08-27 19:19:23 +0700

    initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+# Install
+
+```sh
+rm -rf ~/.config/nvim ~/.local/share/nvim ~/.cache/nvim && git clone https://github.com/kidp2h/lazynvim.git ~/.config/nvim --depth 1 && nvim
+```
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash sh
-
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
-rm -rf ~/.cache/nvim
-
-git clone https://github.com/kidp2h/lazynvim.git ~/.config/nvim --depth 1 && nvim
